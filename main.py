30f577011926d32ffb163964ac6c7eaa960b9e59
shubham gugaliya <ssg9404848801@gmail.com>
2025-01-27 11:44:04 +0530

    DAy 24 : created multiple pages using for loop

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ pdf=FPDF(orientation='p',unit='mm',format='A4')
 
 df=pd.read_csv("topics.csv")
 
+for index,row in df.iterrows():
+    pdf.add_page()
 
-pdf.add_page()
+    pdf.set_font(family='Times',style='B',size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0,h=12, txt=row['Topic'], align="L", ln=1 )
+    pdf.line(10,20,200,20)
 
-pdf.set_font(family='Times',style='B',size=12)
-pdf.cell(w=0,h=12, txt='Hello There', align="L", ln=1, border=1 )
-pdf.cell(w=0,h=12, txt='Hi There', align="L", ln=1, border=1 )
 pdf.output("output.pdf")
